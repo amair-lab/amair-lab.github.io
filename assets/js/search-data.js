bb2cd332fa13d5d1bd2b3a9c5d3d621826a48464
Dandelionym <62579469+Dandelionym@users.noreply.github.com>
2025-04-02 01:44:42 +0000

    Deploying to gh-pages from @ amair-lab/amair-lab.github.io@d1670831f717c25c6145ec69115f2c9575b39062 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -38,30 +38,4 @@ ninja.data = [{
           description: "We introduce a hypothesis-validation framework with language model-based multi-agent system for scientific discovery.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/PriM/";
-            },},{
-      id: 'light-theme',
-      title: 'Change theme to light',
-      description: 'Change the theme of the site to Light',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("light");
-      },
-    },
-    {
-      id: 'dark-theme',
-      title: 'Change theme to dark',
-      description: 'Change the theme of the site to Dark',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("dark");
-      },
-    },
-    {
-      id: 'system-theme',
-      title: 'Use system default theme',
-      description: 'Change the theme of the site to System Default',
-      section: 'Theme',
-      handler: () => {
-        setThemeSetting("system");
-      },
-    },];
+            },},];
